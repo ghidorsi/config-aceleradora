0374dc5cabf0f0bf0b4d315870f1de6cf792aee0
Ghilherme Ghidorsi <ghidorsi@thoughtworks.com>
2019-03-15 15:15:01 -0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,10 +1,22 @@
 VSCODE_URL="https://go.microsoft.com/fwlink/?LinkID=760868"
+INTELLIJ_URL="https://download.jetbrains.com/idea/ideaIU-2018.3.5-no-jdk.tar.gz"
+INTELLIJ_FOLDER_NAME="idea-IU-183.5912.21/"
 
-sudo apt install -y git openjdk-11-jdk postgresql postgresql-contrib curl vim tmux
+sudo apt install -y git openjdk-11-jdk postgresql postgresql-contrib curl vim tmux xsel
 
 curl -L -o vscode.deb ${VSCODE_URL}
 
 sudo dpkg -i vscode.deb
+
+curl -L -o idea.tar.gz ${INTELLIJ_URL}
+
+sudo tar -xzf idea.tar.gz
+
+sudo mv ${INTELLIJ_FOLDER_NAME} /opt/idea
+
+sudo chmod +x /opt/idea
+
+sh /opt/idea/bin/idea.sh
 
 code --version
 
