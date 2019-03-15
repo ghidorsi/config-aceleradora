41e37460ae08d75040a48070c0c5807fcc458466
Yasser Rachid <anuar.yasser@gmail.com>
2019-03-15 14:44:17 -0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,4 +1,10 @@
-sudo apt install -y git openjdk-11-jdk
+VSCODE_URL="https://go.microsoft.com/fwlink/?LinkID=760868"
+
+sudo apt install -y git openjdk-11-jdk postgresql postgresql-contrib curl
+
+curl ${VSCODE_URL} -o vscode.deb
+
+sudo dpkg -i vscode.deb
 
 git --version
 
